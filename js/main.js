a1fe5e23184c073d95cd7e54507fd72d12d694c6
buchajgucha <sportnashevse@yandex.ru>
2022-06-24 13:02:34 +0300

    replace jquery to js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 60,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-btn-next",
-      prevEl: ".swiper-btn-prev",
-    },
-    
-  });
+  slidesPerView: 4,
+  spaceBetween: 60,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
+  },
+});
 
-  $('.video__link').on('click', function(){
-    $('.video__play').toggleClass('video__play--active');
-    $('.video__img').toggleClass('video__img--active');
-    if($('.video__play').hasClass('video__play--active')){
-      $('.video__play').trigger('play');
-    
-    }else{$('.video__play').trigger('pause')}
-    
+const videoLink = document.querySelector(".video__link");
+const videoPlay = document.querySelector(".video__play");
+const videoImg = document.querySelector(".video__img");
+
+videoLink.addEventListener("click", function () {
+  videoPlay.classList.toggle("video__play--active");
+  videoImg.classList.toggle("video__img--active");
+  if (videoPlay.classList.contains("video__play--active")) {
+    videoPlay.play();
+  } else {
+    videoPlay.pause();
+  }
 });
